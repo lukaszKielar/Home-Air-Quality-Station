e224b6d7077e552d653c9a81200ac46058942281
lukaszKielar <luk.kielar@gmail.com>
2018-09-08 19:18:39 +0200

    New python functions for PostgreSQL Database added to API

--- python/haqs_api/haqs_api.py
+++ python/haqs_api/haqs_api.py
@@ -12,7 +12,7 @@ import bokeh
 from bokeh import plotting
 from bokeh.models import GeoJSONDataSource, HoverTool
 from bokeh.plotting import figure, show, ColumnDataSource
-from bokeh.tile_providers import CARTODBPOSITRON
+from bokeh.tile_providers import CARTODBPOSITRON, STAMEN_TERRAIN, STAMEN_TONER
 import psycopg2
 
 NoneType = type(None)
@@ -31,14 +31,26 @@ def get_stations():
     return requests.get(stations_request).json()
 
 
-stations = get_stations()
-
-
-def create_stations_gdf(map=False):
+def create_stations_gdf(stations_json, map=False):
     """
     Function returns stations GeoDataFrame.
     Function has been deprecated since PostgreSQL Database
     was added to the project.
+
+    params
+    -------------
+        stations (dict):
+            JSON object requested from GIOŚ API
+
+        map (bool):
+            boolean used for additional output
+            necessary for folium map
+
+    returns
+    -------------
+        stations_df (gpd.GeoDataFrame):
+            geopandas GeoDataFrame with all
+            available air monitoring stations
     """
     stations_dict = {}
     station_ids = []
@@ -46,15 +58,15 @@ def create_stations_gdf(map=False):
     station_lon = []
     station_lat = []
     station_geometries = []
-    
+
     if map:
-        for station in stations:
+        for station in stations_json:
 
             station_ids.append(station["id"])
             station_name.append(station["stationName"])
             station_lon.append(float(station["gegrLon"]))
             station_lat.append(float(station["gegrLat"]))
-            station_geometries.append(Point(float(station["gegrLon"]), 
+            station_geometries.append(Point(float(station["gegrLon"]),
                                             float(station["gegrLat"])))
 
         stations_dict["station_id"] = station_ids
@@ -62,12 +74,12 @@ def create_stations_gdf(map=False):
         stations_dict["latitude"] = station_lat
         stations_dict["longitude"] = station_lon
         stations_dict["geometry"] = station_geometries
-        
+
     else:
-        for station in stations:
+        for station in stations_json:
 
             station_ids.append(station["id"])
-            station_geometries.append(Point(float(station["gegrLon"]), 
+            station_geometries.append(Point(float(station["gegrLon"]),
                                             float(station["gegrLat"])))
 
         stations_dict["station_id"] = station_ids
@@ -79,33 +91,31 @@ def create_stations_gdf(map=False):
     return stations_df
 
 
-stations_df = create_stations_gdf()
-
-
-def create_stations_map():
+def create_stations_map(stations_json):
     """
-    Function returns folium map with GIOS stations in Poland
+    Function returns folium map with GIOS
+    air monitoring stations in Poland
     """
     stations_map = folium.Map([52, 19], zoom_start=6, tiles='Stamen Terrain')
-    
-    stations_df = create_stations_gdf(map=True)
-    
+
+    stations_df = create_stations_gdf(stations_json, map=True)
+
     locations = stations_df[["latitude", "longitude"]]
     locations_list = locations.values.tolist()
-    
+
     for point in range(len(stations_df)):
-        folium.Marker(locations_list[point], 
+        folium.Marker(locations_list[point],
                       popup=stations_df['station_name'][point]).add_to(stations_map)
     
     """FIRST VERSION (without pin description)"""
     # points = folium.features.GeoJson(stations_df.to_json())
     # stations_map.add_child(points)
     # stations_map.add_child(HeatMap([[row["lat"], row["lon"]] for name, row in stations_df.iterrows()]))
-    
+
     return stations_map
 
 
-def create_sensors_df():
+def create_sensors_df(stations_json):
     """
     Function returns sensors DataFrame.
     Function has been deprecated since PostgreSQL Database
@@ -116,7 +126,7 @@ def create_sensors_df():
     sensors_ids = []
     sensors_param = []
 
-    for station in stations:
+    for station in stations_json:
 
         station_id = station["id"]
         sensors = requests.get(sensors_request + str(station_id)).json()
@@ -136,8 +146,8 @@ def create_sensors_df():
     return sensors_df
 
 
-sensors_df = create_sensors_df()
-available_parameters = list(sensors_df.parameter.unique())
+def get_available_parameters(sensors_df):
+    return list(sensors_df.parameter.unique())
 
 
 def get_sensor_readings(row):
@@ -152,13 +162,13 @@ def get_sensor_readings(row):
         data = data_json["values"][count]["value"]
         while isinstance(data, NoneType):
             data = data_json["values"][count]["value"]
-            count += 1 
+            count += 1
         return data
     except:
         return np.NaN
-    
 
-def get_latest_sensors_readings():
+
+def get_latest_sensors_readings(sensors_df):
     """
     Function returns latest available reading for each sensor.
     It uses get_sensor_readings() function.
@@ -166,4 +176,130 @@ def get_latest_sensors_readings():
     sensors_df['data'] = sensors_df.apply(get_sensor_readings, axis=1)
     return sensors_df
 
+
+def get_param_df(stations_df, sensors_df, parameter):
+    """
+    Function returns pandas DataFrame with latest
+    readings for parsed parameter
+    """
+
+    param_df = sensors_df[sensors_df["parameter"] == "{}".format(parameter)]
+    param_df = gpd.GeoDataFrame(pd.merge(param_df, stations_df, on='station_id'))
+    param_df.dropna(inplace=True)
+    param_df.crs = coord_system
+
+    return param_df
+
+
+def show_readings_map(param_df, tile=CARTODBPOSITRON):
+
+    # conversion to World Mercator needed
+    param_df = param_df.to_crs(epsg=3395)
+
+    # normalize data column
+    param_df["data"] = (param_df['data']-param_df['data'].mean())/param_df['data'].std()
+    param_df["data"] = (param_df['data']-param_df['data'].min())/(param_df['data'].max()-param_df['data'].min())
+
+    plotting.output_notebook()
+
+    # reduce differences between readings
+    size = param_df["data"] * 50
+    # get data resolution
+    radii = np.array(size)
+    # create colors list
+    colors = ["#%02x%02x%02x" % (int(r), int(g), int(b)) for r, g, b, _ in 255*mpl.cm.RdYlGn(1-mpl.colors.Normalize()(radii))]
+
+    p = plotting.figure(toolbar_location="left",
+                        plot_width=900,
+                        plot_height=700,
+                        x_axis_type="mercator",
+                        y_axis_type="mercator")
+
+    p.circle(param_df['geometry'].x, param_df['geometry'].y, size=size,
+             fill_color=colors, fill_alpha=0.8, line_color=None)
+
+    p.add_tile(tile)
+
+    plotting.show(p)
+
+
+"""POSTGRESQL PART"""
+
+
+def connect_with_db():
+    """
+    Function connects wirh PostgreSQL DataBase
+    and returns connection object if connection
+    was succesfully established
+    """
+    try:
+        conn = psycopg2.connect("dbname='haqs' user='postgres' host='localhost' password='postgres'")
+        print("Successfully conected with DataBase!")
+        return conn
+    except Exception as e:
+        print(e)
+        print("Unable to connect to the DataBase!")
+        conn.close()
+
+
+def close_db_connection(conn):
+    conn.close()
+    print("Connection has been closed!")
+
+
+def create_postgis_extension(conn):
+    sql = "CREATE EXTENSION postgis;"
+    execute_sql(conn, sql)
+
+
+def execute_sql(conn, sql, *args):
+    """
+    Function helps to deal with DataBase errors
+    which occurs during sql statements execution.
+    It rolls back to savepoint created before
+    statement run.
+    """
+    cur = conn.cursor()
+    cur.execute('SAVEPOINT sp1')  # create database savepoint
+    print("SAVEPOINT created!")
+    try:
+        cur.execute(sql, tuple(args))
+        conn.commit()
+        print("SQL was successfully executed!")
+    except Exception as e:
+        print(e)
+        cur.execute('ROLLBACK TO SAVEPOINT sp1')  # rollback to savepoint
+        print("Rollback to SAVEPOINT!")
+    finally:
+        cur.execute('RELEASE SAVEPOINT sp1')  # release savepoint
+        print("SAVEPOINT released!")
+
+
+def show_database_tables(conn):
+    sql = "SELECT * FROM pg_catalog.pg_tables;"
+    execute_sql(conn, sql)
+
+
+def create_stations_table(conn):
+    sql =   """
+                CREATE TABLE public.stations
+                (
+                    station_id INTEGER PRIMARY KEY,
+                    geom geometry(Point, 4326)
+                );
+            """
+    # To add geometry column to Table following command is needed
+    # SELECT AddGeometryColumn('stations', 'geom', '4326', 'POINT', 2);
+    execute_sql(conn, sql)
+
+
+def db_insert_station(conn, *args):
+    """
+    Function inserts stations with its coordinates to Stations Table
+    """
+    sql =   """
+                INSERT INTO public.stations (station_id, geom)
+                VALUES (%s, ST_SetSRID(ST_MakePoint(%s, %s), 4326));
+            """
+    execute_sql(conn, sql, *args)
 
