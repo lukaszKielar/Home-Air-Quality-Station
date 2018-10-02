521ac96a425b30a0f668109f28dbed37ff5d3f88
lukaszKielar <luk.kielar@gmail.com>
2018-10-02 22:26:39 +0200

    wifi_config.h example added

---
+++ wifi_config_example.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "wifi_name"
+#define WIFI_PASSWORD "wifi_password"
+#define MQTT_SERVER "255.255.255.255"
+#define MQTT_PORT 1880
+#define MQTT_USER "mqtt_user"
+#define MQTT_PASSWORD "mqtt_password"
